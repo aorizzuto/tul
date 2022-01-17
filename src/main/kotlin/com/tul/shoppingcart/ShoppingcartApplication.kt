1027cecade7a581e3f6747f20e00e5ce45f165b2
alejandro.rizzuto <alejandro.rizzuto@naranjax.com>
2022-01-17 11:55:21 -0300

    Project structure

---
+++ src/main/kotlin/com/tul/shoppingcart/ShoppingcartApplication.kt
@@ -0,0 +1,11 @@
+package com.tul.shoppingcart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingcartApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShoppingcartApplication>(*args)
+}
